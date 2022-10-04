295d2f36e3c26cc81b51e11e1fcaa34aebf9c297
Joni Turunen <joni.turunen@triplan.fi>
2022-10-04 22:58:05 +0300

    Add test for impl. one

--- cmd/main/main_test.go
+++ cmd/main/main_test.go
@@ -1,17 +1,16 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
 
 func Test_example(t *testing.T) {
-	t.Run("Constructed with one parameter", func(t *testing.T) {
-		got := "Assertion"
-		expected := "Expected result"
+	t.Run("TestFirstImplementation", func(t *testing.T) {
+		slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+		num := 7
+		got, gotIterations := one(num, slice)
+		expected, expectedIterations := num, 11
 
 		if got != expected {
-			t.Errorf("GOT: %q\nEXPECTED: %q\n", got, expected)
+			t.Errorf("GOT: %q and %q\nEXPECTED: %q and %q", got, gotIterations, expected, expectedIterations)
 		}
 	})
 }
-
