42939b6cc9e2abdc7ce179795f6b1085483c8fa8
Joni Turunen <joni.turunen@triplan.fi>
2022-10-04 22:58:25 +0300

    Call implementations with declared slices

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"log"
+	"go.uber.org/zap"
 )
 
 func main() {
-	log.Println("Im waiting to be implemented")
-} 
+	sugar := initializeLogger().Sugar()
+	sliceOfIntegers := []int{82, 66, 14, 1, 3, 42, 8, 321, 54, 12, 56, 6, 9}
+	sortedSliceOfInts := bubbleSort(sliceOfIntegers)
+
+	// Number to search for
+	num := 66
+
+	// First implementation
+	one, iterations := one(num, sortedSliceOfInts)
+	sugar.Info("One: ", one, ". Iterations: ", iterations)
+	// Second implementation
+	two, iterations := two(num, sortedSliceOfInts)
+	sugar.Info("Two: ", two, ". Iterations: ", iterations)
+}
+
+func initializeLogger() *zap.Logger {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	logger.Debug("Logger initialized")
+
+	return logger
+}
