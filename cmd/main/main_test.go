733e26615dfe809675248c55b34cd37b00406fc0
Joni Turunen <joni.turunen@triplan.fi>
2022-10-05 00:01:31 +0300

    Add tests to 2nd and 3rd impl.

--- cmd/main/main_test.go
+++ cmd/main/main_test.go
@@ -13,4 +13,26 @@ func Test_example(t *testing.T) {
 			t.Errorf("GOT: %q and %q\nEXPECTED: %q and %q", got, gotIterations, expected, expectedIterations)
 		}
 	})
+
+	t.Run("TestSecondImplementation", func(t *testing.T) {
+		slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+		num := 1
+		got, _ := two(num, slice)
+		expected := num
+
+		if got != expected {
+			t.Errorf("GOT: %q \nEXPECTED: %q ", got, expected)
+		}
+	})
+
+	t.Run("TestThirdImplementation", func(t *testing.T) {
+		slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+		num := 6
+		got, _ := three(num, slice)
+		expected := num
+
+		if got != expected {
+			t.Errorf("GOT: %q \nEXPECTED: %q ", got, expected)
+		}
+	})
 }
